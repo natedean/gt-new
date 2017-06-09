b5e11cd8ce2f5ee4189c3c58a3e994f623d8d083
Nate Dean <nate.dean@selectquote.com>
2017-06-09 16:13:32 -0500

    Deleted a bunch of old stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import AnimatedLogo from './AnimatedLogo';
-import logo from './logo.svg';
 import About from './components/about';
 
 import './css/normalize.css';
 import './css/skeleton.css';
 import './css/index.css';
-
-import './App.css';
 
 class App extends Component {
   render() {
