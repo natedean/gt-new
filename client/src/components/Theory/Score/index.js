3c734a96adf1ddf5a117dee23d96f81e68cc862e
Nate Dean <nate.dean@selectquote.com>
2017-07-17 09:10:16 -0500

    Adds d3, to later pull linear scale functions, and more stuff

--- client/src/components/Theory/Score/index.js
+++ client/src/components/Theory/Score/index.js
@@ -2,25 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './index.css';
 
-const Score = ({score, userData, statsData}) => (
-  <div className="score">
-    <div>
-      My score: {userData.totalCorrect}. My attempts: {userData.totalCorrect + userData.totalIncorrect}
+const Score = ({score, userData, statsData}) => {
+
+
+  return (
+    <div className="score">
+      <div>
+        My score: {userData.totalCorrect}. My attempts: {userData.totalCorrect + userData.totalIncorrect}
+      </div>
+      <div>
+        Avg score: {statsData.avgTotalCorrect}
+      </div>
+
+
+
+      {/*NO SCALING*/}
+      <div>
+        My skill:
+        <svg viewBox="0 0 100 2" className="score__myScoreChart" style={{transition: '3s all'}}>
+          <line x1="1" x2={userData.correctRatio * 100} y1="1" y2="1" />
+        </svg>
+        {userData.correctRatio}
+      </div>
+      <div>
+        Avg skill:
+        <svg viewBox="0 0 100 2" className="score__myScoreChart">
+          <line x1="1" x2={statsData.avgCorrectRatio * 100} y1="1" y2="1" />
+        </svg>
+        {statsData.avgCorrectRatio}
+      </div>
+
+
     </div>
-    <div>
-      Avg score: {statsData.avgTotalCorrect}
-    </div>
-    <div>
-      My skill: {userData.correctRatio}
-    </div>
-    <div>
-      Avg skill: {statsData.avgCorrectRatio}
-    </div>
-    <svg viewBox="0 0 10 2" className="score__myScoreChart">
-      <line x1="1" x2="5" y1="1" y2="1" />
-    </svg>
-  </div>
-);
+  );
+};
 
 
 export default Score;
