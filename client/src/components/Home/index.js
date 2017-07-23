f1ba04602fafd9095323f4cf0d60bcd6a8da8e56
Nate Dean <nate.dean@selectquote.com>
2017-07-23 12:55:12 -0500

    Changes home text, adds correct Title tag to app

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -6,7 +6,8 @@ export default () => (
       <div className="text-center">
           <h3>Get Better Faster</h3>
           <h5>Learn. Play games. Be awesome.</h5>
-          <p>Been working hard on some great new stuff.  For now, give the <Link to="/games/theory" >Music Theory Game</Link> a try.</p>
+        <p>GuitarThinker is currently under construction. We are working hard to build some great new games. </p>
+        <p>For now, give the <Link to="/games/theory" >Music Theory Game</Link> a try.</p>
       </div>
   </div>
 );
