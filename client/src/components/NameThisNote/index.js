76fdf6b17070fcefa11bb8a3718a9223d04cd2dc
Nate Dean <nate.dean@selectquote.com>
2017-11-04 22:15:42 -0500

    Good progress on NameThisNote game flow

--- client/src/components/NameThisNote/index.js
+++ client/src/components/NameThisNote/index.js
@@ -6,29 +6,87 @@ import './index.css';
 class StaffNote extends Component {
 
   height = 120;
+  questionResetTime = 1000;
 
   state = {
-    currNote: notes[Math.floor(Math.random() * notes.length)]
+    currNote: notes[Math.floor(Math.random() * notes.length)],
+    answeredNoteNames: []
+  };
+
+  answerClickHandler = (e) => {
+    const answer = e.target.innerText;
+
+    // for now, leave this simple, probably refactor at some point
+    this.setLimboState(this.isCurrNote(answer));
+  };
+
+  setLimboState = (wasLastAnswerCorrect) => {
+    const currAnsweredNote = this.state.currNote;
+    const hasAnsweredAll = this.state.answeredNoteNames.length === notes.length - 1;
+
+    this.setState((prevState) => ({
+      isLimbo: true,
+      wasLastAnswerCorrect,
+      answeredNoteNames: hasAnsweredAll ? [] : [...prevState.answeredNoteNames, currAnsweredNote.name]
+    }));
+
+    setTimeout(() => {
+      this.setState(() => ({
+        isLimbo: false,
+        currNote: this.findRandUnansweredNote()
+      }))
+    }, this.questionResetTime);
+  };
+
+  computeButtonClassName = (answer) => {
+    if (!this.state.isLimbo || !this.isCurrNote(answer)) return 'answerButton';
+
+    return `answerButton answerButton--${this.state.wasLastAnswerCorrect ? 'correct' : 'incorrect'}`;
+  };
+
+  isCurrNote = (note) => {
+    return note === this.state.currNote.name.charAt(0);
+  };
+
+  findRandUnansweredNote = () => {
+    const remainingNotes = notes.filter(({name}) => !this.state.answeredNoteNames.includes(name));
+
+    return remainingNotes[Math.floor(Math.random() * remainingNotes.length)];
   };
 
   render() {
+    const {isLimbo, wasLastAnswerCorrect} = this.state;
+
     return (
       <div className="staffNote text-center">
-        <h4>What note is this? {this.state.currNote.name}</h4>
         <div className="text-center">
-        <Staff>
-          <Motion style={{ cy: spring(this.height - this.state.currNote.yOffset - 5, { stiffness: 100, damping: 20}), opacity: spring(1) }}>
-            {(style) => (
-              <ellipse className="staff--quarterNoteBody"
-                       cx="60"
-                       cy={style.cy}
-                       rx="6"
-                       ry="5"
-                       style={{ opacity: style.opacity }}
-              />
-            )}
-          </Motion>
-        </Staff>
+          <h4>What note is this?</h4>
+          {isLimbo && <span style={{position: 'absolute'}}>{wasLastAnswerCorrect ? 'Correct!' : 'Incorrect'} {this.state.currNote.name}</span>}
+          <Staff>
+            <Motion
+              style={{ cy: spring(this.height - this.state.currNote.yOffset - 5, { stiffness: 100, damping: 14}),
+                        opacity: spring(1) }}>
+              {(style) => (
+                <ellipse className="staff--quarterNoteBody"
+                         cx="60"
+                         cy={style.cy}
+                         rx="6"
+                         ry="5"
+                         style={{ opacity: style.opacity }}
+                />
+              )}
+            </Motion>
+          </Staff>
+          <div className="buttonGroup" onClick={this.answerClickHandler}>
+            <button disabled={isLimbo} className={this.computeButtonClassName('A')}>A</button>
+            <button disabled={isLimbo} className={this.computeButtonClassName('B')}>B</button>
+            <button disabled={isLimbo} className={this.computeButtonClassName('C')}>C</button>
+            <button disabled={isLimbo} className={this.computeButtonClassName('D')}>D</button>
+            <button disabled={isLimbo} className={this.computeButtonClassName('E')}>E</button>
+            <button disabled={isLimbo} className={this.computeButtonClassName('F')}>F</button>
+            <button disabled={isLimbo} className={this.computeButtonClassName('G')}>G</button>
+          </div>
+
       </div>
   </div>
     )
