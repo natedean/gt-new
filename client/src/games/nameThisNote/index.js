12b0cf7a1b31848a2ed1ae05816ca4233d4fd2c4
Nate Dean <nate.dean@selectquote.com>
2017-11-16 23:01:40 -0600

    A bunch of cool annyang voice recognition added to nameThisNote game

--- client/src/games/nameThisNote/index.js
+++ client/src/games/nameThisNote/index.js
@@ -1,16 +1,89 @@
 import React, {Component} from 'react';
 import {Motion, spring} from 'react-motion';
 import Staff, {notes} from '../../components/Staff/index';
+import annyang from 'annyang';
 import './index.css';
+
+navigator.getUserMedia  = navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia;
+
+const commandMap = {
+  a: ['a', 'eh', 'hey', 'hay', 'AAA'],
+  b: ['b', 'be','bee','p','bea'],
+  c: ['c', 'see','sea','SI','ce'],
+  d: ['d','dee','di','dd','dee dee'],
+  e: ['e','e e','e r','eat','eeh','e a'],
+  f: ['f','eff','ff','f f','f e'],
+  g: ['g','ge','chi','qi','gee','jeep']
+};
+
+const handleAnnyangResult = (possibleAnswerStrings, matchCallback) => {
+  const commandMapKeys = Object.keys(commandMap);
+
+  for (let i = 0; i < possibleAnswerStrings.length; i++) {
+    const possibleAnswer = possibleAnswerStrings[i].toLowerCase().trim();
+
+    for (let j = 0; j < commandMapKeys.length; j++) {
+      const key = commandMapKeys[j];
+
+      const isMatch = commandMap[key].some(x => {
+        return x === possibleAnswer;
+      });
+
+      if (isMatch) {
+        matchCallback(key);
+        return;
+      }
+    }
+  }
+};
 
 class StaffNote extends Component {
 
   height = 120;
-  questionResetTime = 1000;
+  questionResetTime = 2000;
 
   state = {
     currNote: notes[Math.floor(Math.random() * notes.length)],
-    answeredNoteNames: []
+    answeredNoteNames: [],
+    voiceRecognitionMessage: false
+  };
+
+  componentDidMount() {
+    this.setState(() => ({
+      voiceRecognitionMessage: this.setDefaultVoiceRecognitionMessage()
+    }));
+
+    // Start listening.
+    if (!annyang) return;
+
+    annyang.start();
+
+    const hellFunc = () => console.log('hello!');
+
+    const commands = {'hello': hellFunc, 'howdy': hellFunc, 'hi': hellFunc};
+
+    annyang.addCommands(commands);
+
+    annyang.debug();
+
+    annyang.addCallback('result', (result) => handleAnnyangResult(result, this.annyangHandler));
+  }
+
+  componentWillUnmount() {
+    if (annyang) {
+      annyang.abort();
+    }
+  }
+
+  annyangHandler = (matchedAnswer) => {
+    this.setState(() => ({
+      voiceRecognitionMessage: `What was heard: ${matchedAnswer.toUpperCase()}`
+    }));
+
+    this.setLimboState(this.isCurrNote(matchedAnswer));
   };
 
   answerClickHandler = (e) => {
@@ -18,6 +91,10 @@ class StaffNote extends Component {
 
     // for now, leave this simple, probably refactor at some point
     this.setLimboState(this.isCurrNote(answer));
+  };
+
+  setDefaultVoiceRecognitionMessage = () => {
+    return !!annyang ? 'Voice recognition ready' : 'No voice recognition available';
   };
 
   setLimboState = (wasLastAnswerCorrect) => {
@@ -33,7 +110,8 @@ class StaffNote extends Component {
     setTimeout(() => {
       this.setState(() => ({
         isLimbo: false,
-        currNote: this.findRandUnansweredNote()
+        currNote: this.findRandUnansweredNote(),
+        voiceRecognitionMessage: this.setDefaultVoiceRecognitionMessage()
       }))
     }, this.questionResetTime);
   };
@@ -45,7 +123,7 @@ class StaffNote extends Component {
   };
 
   isCurrNote = (note) => {
-    return note === this.state.currNote.name.charAt(0);
+    return note.toLowerCase() === this.state.currNote.name.charAt(0).toLowerCase();
   };
 
   findRandUnansweredNote = () => {
@@ -60,6 +138,7 @@ class StaffNote extends Component {
     return (
       <div className="staffNote text-center">
         <div className="text-center">
+          <h4>{this.state.voiceRecognitionMessage}</h4>
           <h4>What note is this?</h4>
           {isLimbo && <span style={{position: 'absolute'}}>{wasLastAnswerCorrect ? 'Correct!' : 'Incorrect'} {this.state.currNote.name}</span>}
           <Staff>
